4a3607b96f9c1b76bb32b83152afd7844ca346f3
Humaiun-Manik <humaiunkobirr@gmail.com>
2022-02-15 23:57:48 +0600

    added resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,6 +5,6 @@ export const headerData = {
     title: "Web Developer",
     desciption: "I am an experienced and expert web application developer with having 1 years of experience.",
     image: 'https://i.ibb.co/DppJRjf/20211206-1426251.jpg',
-    resumePdf: 'https://drive.google.com/file/d/1G1pRmhloCjcQu7K6U7kIisKLlS1y1bNK/view?usp=sharing',
+    resumePdf: resume,
     logoH: logo
 }
